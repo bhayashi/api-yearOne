21a828b6d35788db0d0ae0d2f0c7e5a56d2d1a48
b-hash <brianmhayashi@gmail.com>
2020-11-30 16:56:15 -0800

    (add) boiler plate react files

---
+++ client/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/home" exact component={} />
+      <Route path="/" exact component={} />
+    </Switch>
+  );
+}
+
+export default App;
