c2ba54811862959a855039bc69a75e727b547f7f
b-hash <brianmhayashi@gmail.com>
2020-12-01 15:48:52 -0800

    (add) home container to house search components

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import HomeContainer from './containers/HomeContainer';
 
 function App() {
   return (
-    // <Switch>
-    //   <Route path="/home" exact component={} />
-    //   <Route path="/" exact component={} />
-    // </Switch>
-    <h1>Hello from App.tsx</h1>
+    <Switch>
+      <Route path="/" exact component={HomeContainer} />
+      {/* <Route path="/" exact component={} /> */}
+    </Switch>
+    // <h1>Hello from App.tsx</h1>
   );
 }
 
---
+++ client/containers/HomeContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeContainer = () => (
+  <div id="home-container">
+    <h1>HomeContainer</h1>
+  </div>
+);
+
+export default HomeContainer;
