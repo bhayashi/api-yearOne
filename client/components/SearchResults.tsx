e433deb839d4199acab57f4b4db9547a93f2c0e6
b-hash <brianmhayashi@gmail.com>
2020-12-01 17:12:36 -0800

    (add) Search Results component

---
+++ client/components/SearchResults.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const SearchResults = () => {
+  const [searchText, setSearchText] = useState('');
+
+  return (
+    <div>
+      <h2>Search Results</h2>
+    </div>
+  );
+};
+
+export default SearchResults;
