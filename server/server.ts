9bb86c6f14d34ba361efc52c8d4478e4b65fbb6f
b-hash <brianmhayashi@gmail.com>
2020-12-01 21:08:21 -0800

    (delete) removes unused axios package

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,12 @@
 import express from 'express';
 import path from 'path';
-import cors from 'cors';
-import axios from 'axios';
+// import cors from 'cors';
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const PORT = 3000;
 const app = express();
-app.use(cors());
-
-const omdbKey: string =
-  typeof process.env.OMDB_KEY === 'string' ? process.env.OMDB_KEY : '';
+// app.use(cors());
 
 app.use(express.static('public'));
 
@@ -19,5 +15,7 @@ app.use('/build', express.static(path.resolve(__dirname, '../../build')));
 app.get('/', (_req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../../public/index.html'));
 });
+
+app.use((_req, res) => res.sendStatus(404));
 
 app.listen(PORT, () => console.log('Listening on 3000'));
